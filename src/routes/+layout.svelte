4e46109bd4e8a67067f28f86ee5ed5335516f3b9
Mr <mrmylad10@gmail.com>
2023-07-03 19:07:17 +0530

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import "$lib/index.css";
+    let app: HTMLDivElement;
+    let darkModeEnabled: boolean = false;
+    const toggleDarkMode: () => void = () => {
+        darkModeEnabled = !darkModeEnabled;
+    };
+    $: appDivClass = `${darkModeEnabled ? "dark bg-gray-800" : "bg-gray-50"}`;
+</script>
+
+<div id="app" class={appDivClass} bind:this={app}>
+    <nav class="mb-1 text-right w-screen text-black dark:text-white bg-white dark:bg-gray-950">
+        <button class="border border-gray-950" on:click={toggleDarkMode}>{darkModeEnabled ? "Light Mode" : "Dark Mode"}</button>
+    </nav>
+    <slot />
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,4 @@
+<div id="mrmylad.ml" class="bg-white dark:bg-gray-950 h-screen">
+    <h1 class="text-center font-bold text-3xl text-black dark:text-white">Hello World!</h1>
+    <h3 class="text-center text-black dark:text-white">There's more to come soon!</h3>
+</div>
