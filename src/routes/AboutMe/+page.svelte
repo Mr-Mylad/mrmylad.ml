0a017bba3a553886fcfa3f52cf85877b8b89deec
Mr <mrmylad10@gmail.com>
2023-07-08 02:14:06 +0530

    Added an About Me page

---
+++ src/routes/AboutMe/+page.svelte
@@ -0,0 +1,9 @@
+<svelte:head>
+    <title>About Me | mrmylad.ml</title>
+</svelte:head>
+<h1 class="header-1 mb-4">Hello! My name is Mr. Mylad!</h1>
+<hr class="h-fit mb-4 border-gray-800 dark:border-slate-300 border-y">
+<h3 class="header-3">I am a hobbyist web and game dev.</h3>
+<h3 class="header-3">I make websites with JavaScript / Svelte (Kit)</h3>
+<h3 class="header-3">Apart from SvelteKit, I also know React, vanilla HTML, CSS, JS, as well as the Unity Game Engine.</h3>
+<h3 class="header-3">Although, the truth is I still don't understand art, like, I can't draw anything at all 🤷.</h3>
