d1e83ce6ccbc248184ddabb317faac7622927eef
Mr <mrmylad10@gmail.com>
2023-07-08 01:33:33 +0530

    Rewrote the root +page.svelte to be more readable and have a title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
-<div id="mrmylad.ml" class="bg-white dark:bg-gray-950 h-screen">
-    <h1 class="text-center font-bold text-3xl text-black dark:text-white">Hello World!</h1>
-    <h3 class="text-center text-black dark:text-white">There's more to come soon!</h3>
-</div>
+<svelte:head>
+    <title>Home | mrmylad.ml</title>
+</svelte:head>
+<h1 class="header-1">Hello World! 👋</h1>
+<h3 class="header-3">There's more to come soon!</h3>
